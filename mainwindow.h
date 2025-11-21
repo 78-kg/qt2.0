e1c49160c4c973567d63b50b6d5dd48b58ecdeb6
78-kg <776149020@qq.com>
2025-11-21 13:37:00 +0800

    修复mainwindow.cpp中的absoluteFilePath错误，完善记事本功能

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +18,25 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionAbout_triggered();
+
+    void on_actionFind_triggered();
+
+    void on_actionReplace_triggered();
+
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionSaveAs_triggered();
+
 private:
     Ui::MainWindow *ui;
+
+    QLabel statusCursorLabel;
+    QLabel statusLabel;
 };
 #endif // MAINWINDOW_H
